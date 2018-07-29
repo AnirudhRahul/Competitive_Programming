39675a6496d6fef2fbb1667b116bbe89ba45924f
AnirudhRahul <anirudhrahul@anjurahul.com>
2018-07-29 11:47:25 +0200

    update

--- USACO_Gold/src/circularBarn.java
+++ USACO_Gold/src/circularBarn.java
@@ -9,10 +9,24 @@ import java.util.StringTokenizer;
  * Created by user on 7/29/2018.
  */
 public class circularBarn {
+    public static int[] cumSum(int start){
+        int[] cumulative=new int[len];
+        cumulative[0]=cows[start]-1;
+        for(int i=1;i<len;i++){
+            cumulative[i]=cumulative[i-1]+cows[(start+i)%len]-1;
+        }
+        int[] diff=new int[len-1];
+        for(int i=0;i<len-1;i++){
+            diff[i]=cumulative[i+1]-cumulative[i];
+        }
+        return cumulative;
+    }
+    public static int len;
+    public static int[] cows;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int len=Integer.parseInt(br.readLine());
-        int[] cows=new int[len];
+        len=Integer.parseInt(br.readLine());
+        cows=new int[len];
         int[] cumulative=new int[len];
         for(int i=0;i<len;i++){
             cows[i]=Integer.parseInt(br.readLine());
@@ -21,7 +35,6 @@ public class circularBarn {
             else
                 cumulative[i]+=cows[i]-1;
         }
-        System.out.println(Arrays.toString(cumulative));
 
         int index=len;
         int sum=0;
@@ -33,9 +46,15 @@ public class circularBarn {
         }
         else
             index=0;
-        System.out.println(Arrays.toString(cumulative));
-        System.out.println((index));
-
+//        System.out.println(Arrays.toString(cumulative));
+//        System.out.println(Arrays.toString(cumSum(1)));
+////        System.out.println((index));
+//        System.out.println(Arrays.toString(cumSum(index)));
+        for(int i=0;i<len;i++)
+            System.out.println(Arrays.toString(cumSum(i)));
+        for(int i=0;i<len;i++)
+            cows[i]--;
+        System.out.println(Arrays.toString(cows));
 
     }
 }
