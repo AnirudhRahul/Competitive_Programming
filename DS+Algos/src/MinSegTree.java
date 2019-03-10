7debe4bdf81166b09eb80694ff42b8c25d5104e0
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-03-10 00:38:41 -0500

    Iterative Segment Tree Implementation

--- DS+Algos/src/MinSegTree.java
+++ DS+Algos/src/MinSegTree.java
@@ -1,117 +1,75 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Function;
+
 /**
  * Created by user on 7/21/2018.
  */
-public class MinSegTree {
-    // Program for range minimum query using segment tree
-    static class SegmentTreeRMQ {
-        int st[]; //array to store segment tree
+abstract class SegTree{
 
-        // A utility function to get minimum of two numbers
-        int minVal(int x, int y) {
-            return (x < y) ? x : y;
-        }
-
-        // A utility function to get the middle index from corner
-        // indexes.
-        int getMid(int s, int e) {
-            return s + (e - s) / 2;
-        }
-
-        /*  A recursive function to get the minimum value in a given
-            range of array indexes. The following are parameters for
-            this function.
-
-            st    --> Pointer to segment tree
-            index --> Index of current node in the segment tree. Initially
-                       0 is passed as root is always at index 0
-            ss & se  --> Starting and ending indexes of the segment
-                         represented by current node, i.e., st[index]
-            qs & qe  --> Starting and ending indexes of query range */
-        int RMQUtil(int ss, int se, int qs, int qe, int index)
-        {
-            // If segment of this node is a part of given range, then
-            // return the min of the segment
-            if (qs <= ss && qe >= se)
-                return st[index];
-
-            // If segment of this node is outside the given range
-            if (se < qs || ss > qe)
-                return Integer.MAX_VALUE;
-
-            // If a part of this segment overlaps with the given range
-            int mid = getMid(ss, se);
-            return minVal(RMQUtil(ss, mid, qs, qe, 2 * index + 1),
-                    RMQUtil(mid + 1, se, qs, qe, 2 * index + 2));
-        }
-
-        // Return minimum of elements in range from index qs (quey
-        // start) to qe (query end).  It mainly uses RMQUtil()
-        int RMQ(int n, int qs, int qe)
-        {
-            // Check for erroneous input values
-            if (qs < 0 || qe > n - 1 || qs > qe) {
-                System.out.println("Invalid Input");
-                return -1;
-            }
-
-            return RMQUtil(0, n - 1, qs, qe, 0);
-        }
-
-        // A recursive function that constructs Segment Tree for
-        // array[ss..se]. si is index of current node in segment tree st
-        int constructSTUtil(int arr[], int ss, int se, int si)
-        {
-            // If there is one element in array, store it in current
-            //  node of segment tree and return
-            if (ss == se) {
-                st[si] = arr[ss];
-                return arr[ss];
-            }
-
-            // If there are more than one elements, then recur for left and
-            // right subtrees and store the minimum of two values in this node
-            int mid = getMid(ss, se);
-            st[si] = minVal(constructSTUtil(arr, ss, mid, si * 2 + 1),
-                    constructSTUtil(arr, mid + 1, se, si * 2 + 2));
-            return st[si];
-        }
-
-        /* Function to construct segment tree from given array. This function
-           allocates memory for segment tree and calls constructSTUtil() to
-           fill the allocated memory */
-        void constructST(int arr[], int n)
-        {
-            // Allocate memory for segment tree
-
-            //Height of segment tree
-            int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
-
-            //Maximum size of segment tree
-            int max_size = 2 * (int) Math.pow(2, x) - 1;
-            st = new int[max_size]; // allocate memory
-
-            // Fill the allocated memory st
-            constructSTUtil(arr, 0, n - 1, 0);
-        }
-
-
+    int n; // array size
+    int [] tree;
+    //Value such that func(x,identity) returns x
+    //0 for addition, 1 for multiplication, etc.
+    int identity;
+    public SegTree(int[] in, int identity){
+        this.identity=identity;
+        n=in.length;
+        int maxSize=1;
+        while (maxSize<n)
+            maxSize<<=1;
+        tree = new int[maxSize<<1];
+        build(in);
     }
 
-    // Driver program to test above functions
-    public static void main(String args[]) {
-        int arr[] = {1, 3, 2, 7, 9, 11};
-        int n = arr.length;
-        SegmentTreeRMQ tree = new SegmentTreeRMQ();
+    // Max size of tree
 
-        // Build segment tree from given array
-        tree.constructST(arr, n);
+    // function to build the tree
+    void build( int []arr)
+    {
+        // insert leaf nodes in tree
+        for (int i = 0; i < n; i++)
+            tree[n + i] = arr[i];
 
-        int qs = 1;  // Starting index of query range
-        int qe = 5;  // Ending index of query range
-
-        // Print minimum value in arr[qs..qe]
-        System.out.println("Minimum of values in range [" + qs + ", "
-                + qe + "] is = " + tree.RMQ(n, qs, qe));
+        // build the tree by calculating
+        // parents
+        for (int i = n - 1; i > 0; --i)
+            tree[i]= func(tree[i << 1],
+                    tree[(i << 1) | 1]);
     }
+
+    // function to update a tree node
+    void updateNode(int p, int value)
+    {
+        // set value at position p
+        tree[p + n] = value;
+        p = p + n;
+        // move upward and update parents
+        for (int i = p; i > 1; i >>= 1)
+            tree[i >> 1] = func(tree[i], tree[i^1]);
+    }
+
+    // function to get sum on
+    // interval [l, r)
+    int query(int l, int r) {
+        int res = identity;
+        // loop to find the sum in the range
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if ((l & 1) > 0)
+                res = func(res, tree[l++]);
+            if ((r & 1) > 0)
+                res = func(res, tree[--r]);
+        }
+        return res;
+    }
+
+    //Function being applied on the range examples include:
+    // a+b, identity:0
+    // gcd(a,b), identity:0
+    // a*b, identity:1
+    // max(a,b), identity: Integer.MIN
+    // min(a,b) identity: Integer.MAX
+    abstract int func(int a, int b);
 
 }
